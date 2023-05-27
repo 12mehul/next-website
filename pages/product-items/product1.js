c4cb9c3769078429cdc72e312b9436a63e1d15b0
12mehul <mehul091298@gmail.com>
2023-05-27 12:54:18 +0530

    next-website

---
+++ pages/product-items/product1.js
@@ -0,0 +1,9 @@
+
+
+function product1() {
+  return (
+    <div>product1</div>
+  );
+};
+
+export default product1;
---
+++ pages/product-items/product2.js
@@ -0,0 +1,9 @@
+
+
+function product2() {
+  return (
+    <div>product2</div>
+  );
+};
+
+export default product2;
